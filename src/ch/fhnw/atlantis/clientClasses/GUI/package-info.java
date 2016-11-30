f9deb0f28944fbdb55cb85fc10f79abe906f7b42
NadineNe <nadine.neuenschwander@belponline.ch>
2016-11-30 18:33:44 +0100

    GUI Klassen Update - SubMenu (Language, Rules, Player) - dazugehörige EventHandler

---
+++ src/ch/fhnw/atlantis/clientClasses/GUI/package-info.java
@@ -0,0 +1,18 @@
+package ch.fhnw.atlantis.clientClasses.GUI;
+
+/**
+ * Created by Nadine on 29.11.2016.
+ *
+ * Im Package "clientClasses.GUI" sind alle Klassen der grafischen Benutzeroberfläche auf Seiten des Clients abgelegt.
+ * Sämtliche Grafischen Benutzerobeflächen (alle Klassen mit View, Controller und das Model) sind in der Sprache JavaFX geschrieben.
+ * Das Pattern: Model-View-Controller (MVC) wurde eingesetzt.
+ *
+ *
+ * verwendete Quellen für Bilder und Icons:
+ * Hintergrundbild: https://pixabay.com/ (20.10.2016
+ * Icons für Spieler und Menu: https://material.io/icons/ (29.11.2016)
+ * Flaggen für Sprachen: https://www.iconfinder.com/iconsets/195-flat-flag-psd-icons (29.11.2016)
+ *
+ */
+
+
