7ba48a44d5de263562dce3d7220fe317ecb8c82e
DAT <daniel.tschanz@students.fhnw.ch>
2016-12-05 20:57:45 +0100

    ServerClient Update: Antwort des Server für den Client. Classe ServerClient Handler.

--- src/ch/fhnw/atlantis/serverClasses/Server.java
+++ src/ch/fhnw/atlantis/serverClasses/Server.java
@@ -2,7 +2,9 @@ package ch.fhnw.atlantis.serverClasses;
 
 import com.sun.xml.internal.ws.message.ByteArrayAttachment;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -38,6 +40,7 @@ public class Server {
         socketList.remove(sch);
     }
 
+
     public void forwardMessage(String message, ServerClientHandler sender) {
         Iterator itr = socketList.iterator();
 
@@ -46,6 +49,16 @@ public class Server {
             if (!sender.equals(sch)) {
                 sch.send(message);
             }
+        }
+    }
+
+//forwardmessage
+    public void forwardMessagefromServer(String message) {
+        Iterator itr = socketList.iterator();
+
+        while (itr.hasNext()) {
+            ServerClientHandler sch = (ServerClientHandler) itr.next();
+            sch.send(message);
         }
     }
 
@@ -64,5 +77,20 @@ public class Server {
         Server server = Server.getInstance();
 
         server.startTCP();
+
+        //bufferedreader for server
+        while (true) {
+            try {
+                BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                String message = br.readLine();
+               // System.out.println(message);
+                server.forwardMessagefromServer(message);
+            } catch ( IOException e ) {
+                e.printStackTrace();
+            }
+
+        }
+
+
     }
 }
