f826ce72fab4c816e654f18f9e21d164e6d0ad88
ikealol <richard.kuenzi@gmail.com>
2016-12-14 18:26:19 +0100

    Identification

--- src/ch/fhnw/atlantis/serverClasses/Interpreter.java
+++ src/ch/fhnw/atlantis/serverClasses/Interpreter.java
@@ -1,24 +1,33 @@
 package ch.fhnw.atlantis.serverClasses;
 
+import ch.fhnw.atlantis.globalClasses.models.Player;
+import ch.fhnw.atlantis.serverClasses.serverClasses.GUI.ServerView;
+
+import static ch.fhnw.atlantis.globalClasses.models.Player.*;
+
 /**
  * Created by Richard on 06/12/2016.
  */
 public class Interpreter {
 
-    public void interpretServerMsg(String messagefromClient){
+    public String interpretServerMsg(String messagefromClient){
 
-        String monthString;
+        String returnToClient;
+        Player player = new Player();
 
         switch (messagefromClient) {
-            case "Monday":
-                monthString = "[Monday] specific output from client to server interpreter";
-                System.out.println(monthString);
+            case "PlayerConnected":
+                returnToClient = "[Monday] specific output from client to server interpreter";
+                System.out.println(returnToClient);
                 break;
+            case "DefineIdentity":
+                returnToClient = String.valueOf(player.definePlayerIdentity());
+                System.out.println("DefineIdentity: " + returnToClient);
             default:
-                monthString = "Default output from client to server interpreter";
-                System.out.println(monthString);
+                returnToClient = "Default Output";
+                //System.out.println(returnToClient);
         }
-
+        return returnToClient;
     }
 
 }
